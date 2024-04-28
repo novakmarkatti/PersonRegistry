8a3eb28d39c949a1288847529b41cdf8b1f0cb0b
novakmarkatti <novakmarkatti@gmail.com>
2024-04-28 15:12:17 +0200

    initial data modified

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,10 +2,10 @@ INSERT INTO Persons (person_name) VALUES ('PersonA');
 INSERT INTO Persons (person_name) VALUES ('PersonB');
 INSERT INTO Persons (person_name) VALUES ('PersonC');
 
-INSERT INTO Addresses (person_id, address_type, country, state, city, street, zip_code) VALUES 
-((SELECT person_id FROM Persons WHERE person_name = 'PersonA'), 'permanent', 'HUNGARY', 'stateA', 'cityA', 'streetA1', 1111),
-((SELECT person_id FROM Persons WHERE person_name = 'PersonA'), 'temporary', 'HUNGARY', 'stateA', 'cityA', 'streetA2', 2222),
-((SELECT person_id FROM Persons WHERE person_name = 'PersonB'), 'temporary', 'HUNGARY', 'stateB', 'cityB', 'streetB3', 3333);
+INSERT INTO Addresses (person_id, address_type, address_info) VALUES 
+((SELECT person_id FROM Persons WHERE person_name = 'PersonA'), 'permanent', '1111 A Country A street'),
+((SELECT person_id FROM Persons WHERE person_name = 'PersonA'), 'temporary', '2222 A Country A street'),
+((SELECT person_id FROM Persons WHERE person_name = 'PersonB'), 'temporary', '3333 B Country B street');
 
 INSERT INTO Contacts (person_id, contact_type, contact_info) VALUES 
 ((SELECT person_id FROM Persons WHERE person_name = 'PersonA'), 'email', 'asd@asd.com'),
